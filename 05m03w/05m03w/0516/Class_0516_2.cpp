858e3206226b4ef11ba0ab20b81c37a19bfbfb4c
HwaHyang <me@hwahyang.space>
2022-05-18 22:54:44 +0900

    20220518a
    수업내용 백업

---
+++ 05m03w/05m03w/0516/Class_0516_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Player_0516_2 {
+private/*생략가능(기본값)*/:
+	long exp = 0L;
+
+public:
+	int level = 0;
+
+	void move() { }
+	void attack();
+};
+
+void Player_0516_2::attack() {
+	cout << "attack" << endl;
+}
+
+int main_0516_2() {
+	Player_0516_2 p1;
+	p1.level = 1;
+	p1.attack();
+
+	return 0;
+}
---
+++ 05m03w/05m03w/0518/Test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+class Player_0518_2 {
+	int level;
+	string name;
+
+public:
+	Player_0518_2(string _name, int _level) : name(_name), level(_level) {}
+	int GetLevel() { return level; }
+	string GetName() { return name; }
+};
+
+int main_0518_2() {
+	Player_0518_2 playerList[3] = { Player_0518_2("가", 10), Player_0518_2("나", 50), Player_0518_2("다", 30)};
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		cout << "이름: " << playerList[i].GetName() << "\t레벨: " << playerList[i].GetLevel() << endl;
+	}
+
+	return 0;
+}
