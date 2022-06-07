fb9019950c7683e46d9c8f5dd5220779a855a74e
HwaHyang <me@hwahyang.space>
2022-06-07 22:56:51 +0900

    20220607a
    수업내용 백업

---
+++ 06m02w/06m02w/0607/Class.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+using namespace std;
+
+int Compare(int n1, int n2) { return n1 - n2; } // C
+
+class SortRule_0607_1 { // C++
+public:
+	virtual bool operator()(int num1, int num2) const = 0;
+};
+class AscendingSort_0607_1 : public SortRule_0607_1 {
+public:
+	bool operator()(int num1, int num2) const {
+		if (num1 > num2) return true;
+		else return false;
+	}
+};
+class DescendingSort_0607_1 : public SortRule_0607_1 {
+public:
+	bool operator()(int num1, int num2) const {
+		if (num1 < num2) return true;
+		else return false;
+	}
+};
+
+class DataStorage_0607_1 { // for int data
+	int* arr;
+	int idx;
+	const int MAX_LEN;
+public:
+	DataStorage_0607_1(int arrlen) :idx(0), MAX_LEN(arrlen) {
+		arr = new int[MAX_LEN];
+	}
+
+	void AddData(int num) {
+		if (MAX_LEN <= idx) {
+			return;
+		}
+		arr[idx++] = num;
+	}
+
+	void Show() {
+		for (int i = 0; i < idx; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void Sort(int (*p)(int, int)) { // Bubble Sort, 제일 간단하고 느린 방법
+		for (int i = 0; i < (idx - 1); i++) {
+			for (int j = 0; j < (idx - 1) - i; j++) {
+				if (p(arr[j], arr[j + 1]) > 0) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	void Sort(const SortRule_0607_1& func) { // 함수 객체로 받기
+		for (int i = 0; i < (idx - 1); i++) {
+			for (int j = 0; j < (idx - 1) - i; j++) {
+				if (func(arr[j], arr[j + 1])) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+};
+
+int main_0607_1() {
+	DataStorage_0607_1 arr(5);
+
+	arr.AddData(10);
+	arr.AddData(30);
+	arr.AddData(12);
+	arr.AddData(20);
+	arr.AddData(50);
+
+	arr.Sort(Compare);
+	arr.Sort(DescendingSort_0607_1());
+
+	arr.Show();
+
+	return 0;
+}
---
+++ 06m02w/06m02w/0607/TemplateFunction.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+ * Generic Programming(일반화 프로그래밍)
+ * 템플릿 하나로 여러 타입을 받을 수 있는 코드를 짜는 것 (매개변수 다형성)
+ * 
+ * 아래는 함수 템플릿(함수를 만들 수 있는 템플릿)
+ * 
+ * 컴파일러가 함수 템플릿으로 만든 함수는 *템플릿 함수*(????????)
+ * 템플릿을 인스턴스화(구체화)한다고 부름.
+ * 
+ * *** 인터프리터는 템플릿 안쪽에 관여를 하지 못함 ***
+ * 진짜 대놓고 문법 오류를 내지 않는 한, 빌드 전까지 잡아내지 못함.
+ */
+template <typename T> // 무지성으로 T 때려박으면 모르니까 템플릿이라고 정의하기
+T myAbs_0607_2(T num) {
+	if (num < 0) num = -num;
+	return num;
+}
+
+int main_0607_2() {
+	int a = myAbs_0607_2(-2);
+	double b = myAbs_0607_2(-2.0);
+
+	// 내부 처리 방식
+	//int a = myAbs_0607_2<int>(-2);
+	//double b = myAbs_0607_2<double>(-2.0);
+
+	cout << a << endl;
+	cout << b << endl;
+
+	return 0;
+}
