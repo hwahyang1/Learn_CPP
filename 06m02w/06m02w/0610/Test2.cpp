f518a9f9c4bf540e49303d16efccccfa81f32e7e
HwaHyang <me@hwahyang.space>
2022-06-10 23:02:33 +0900

    20220610a
    수업내용 백업

---
+++ 06m02w/06m02w/0610/Test2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(int* p) {
+	for (int i = 0; i < 3; i++) {
+		cout << p[i] << endl;
+		//cout << *(p + i) << endl;
+	}
+}
+
+int main_0610_3() {
+	int arr[3] = { 1, 2, 3 };
+	func(arr);
+
+	return 0;
+}
---
+++ 06m02w/06m02w/0610/Vector.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main_0610_1() {
+	int arr[5] = { 1, 2, 3, 4, 5 };
+
+	vector<int> v = { 1, 2, 3, 4, 5 };
+	vector<double> v1 = { 1.5, 1.0 };
+
+	v.push_back(1);
+	v.pop_back();
+	cout << v.size() << endl;
+	cout << v[3] << endl;
+
+	cout << endl;
+
+	vector<int>::iterator iter; // Pointer
+	iter = v.begin();
+	cout << *iter << endl;
+	iter++;
+	cout << *iter << endl;
+	*iter = 5;
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+	cout << endl;
+
+	sort(v.begin(), v.end(), greater<int>());
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+	return 0;
+}
