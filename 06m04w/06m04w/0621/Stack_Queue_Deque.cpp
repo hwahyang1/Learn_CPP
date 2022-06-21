8a03418741d8383366b393fc70e35b6a662592a3
HwaHyang <me@hwahyang.space>
2022-06-21 23:00:36 +0900

    20220621a
    수업내용 백업

---
+++ 06m04w/06m04w/0621/Stack_Queue_Deque.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+
+int main_0621_2() {
+	stack<int> st;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.pop(); // 3
+	st.pop(); // 2
+	st.pop(); // 1
+
+	queue<int> qu;
+	st.push(1);
+	st.push(2);
+	st.push(3);
+	st.pop(); // 1
+	st.pop(); // 2
+	st.pop(); // 3
+
+	deque<int> dq;
+	for (int i = 0; i < 5; i++) {
+		dq.push_back(i + 1);
+	}
+
+	dq.push_front(100);
+	dq.push_front(200);
+
+	deque<int>::iterator iter;
+	for (iter = dq.begin(); iter != dq.end(); iter++) {
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ 06m04w/06m04w/0621/Vector2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Player_0621_1 {
+	int level;
+public:
+	Player_0621_1() : level(1) {};
+	Player_0621_1(int l) : level(l) {};
+	Player_0621_1(const Player_0621_1& a) :level(a.level) {};
+	int GetLevel() { return level; }
+};
+
+int main_0621_1() {
+	vector<Player_0621_1> players = { Player_0621_1(10), Player_0621_1(4), Player_0621_1(35) };
+	for (auto &e:players)
+	{
+		cout << e.GetLevel() << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
