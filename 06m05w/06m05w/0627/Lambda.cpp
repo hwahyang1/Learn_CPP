f757076ce9a7ca5f6f71cdca1726e55da6c0fcd1
HwaHyang <me@hwahyang.space>
2022-06-27 22:47:45 +0900

    20220627a
    수업내용 백업

---
+++ 06m05w/06m05w/0627/Lambda.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main_0627_1() {
+	auto a1 = 3;
+
+	cout << [a1]() { return a1 + 1; }() << endl;
+	cout << [&a1](auto i) { a1 = i; return a1; }(11) << endl; // 되도록 참조로 안받는게 안전함
+
+	cout << endl;
+
+	int level = 9;
+
+	cout << boolalpha;
+	cout << [level]() -> bool { if (level > 10) return true; return false; }() << endl;
+
+	cout << endl;
+
+	cout << []() -> string { return "Hello World"; } () << endl;
+
+	cout << endl;
+
+	int number;
+	cin >> number;
+
+	cout << [number]() mutable -> int { return number += 10; }() << endl;
+	
+	return 0;
+}
---
+++ 06m05w/06m05w/0627/Lambda2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main_0627_2() {
+	vector<int> v = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	for_each(v.begin(), v.end(), [](const int& n) { if (n % 2 == 0) cout << n << " "; });
+
+	return 0;
+}
