ef6ad5cec4b521ee5adaa3271142c3e052389733
HwaHyang <me@hwahyang.space>
2022-07-01 22:54:18 +0900

    20220701a
    수업내용 백업

---
+++ 06m05w/06m05w/0701/Thread.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void func1_0701_1() {
+	for (int i = 0; i < 10; i++) { std::cout << "쓰레드 1 작동중!" << endl; }
+}
+void func2_0701_1() {
+	for (int i = 0; i < 10; i++) { std::cout << "쓰레드 2 작동중!" << endl; }
+}
+void func3_0701_1() {
+	for (int i = 0; i < 10; i++) { std::cout << "쓰레드 3 작동중!" << endl; }
+}
+
+
+int main_0701_1() {
+	cout << thread::hardware_concurrency() << endl; // 논리프로세서 개수
+	cout << this_thread::get_id() << endl; // 쓰레드 ID
+
+	cout << endl;
+
+	thread t1(func1_0701_1);
+	thread t2(func2_0701_1);
+	thread t3(func3_0701_1);
+
+	for (int i = 0; i < 10; i++) { std::cout << "main 작동중!" << endl; }
+
+	// 종료 대기 (작동 중인데 main이 끝나버리면 답없음)
+	t1.join();
+	t2.join();
+	t3.join();
+
+	// 대기 X (작동 중이든 말든 main 그냥 종료함)
+	/*t1.detach();
+	t2.detach();
+	t3.detach();*/
+
+	cout << "main 종료" << endl;
+	
+	return 0;
+}
---
+++ 06m05w/06m05w/0701/Thread2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <thread>
+#include <vector>
+using namespace std;
+
+void worker_0701_2(vector<int>::iterator start, vector<int>::iterator end, int* result) {
+	int sum = 0;
+	for (auto itr = start; itr < end; itr++) {
+		sum += *itr;
+	}
+	*result = sum;
+
+	thread::id this_id = this_thread::get_id();
+	printf("쓰레드 %x에서 %5d부터 %5d까지 계산한 결과 : %d\n", this_id, *start, *(end - 1), sum);
+}
+
+int main_0701_2() {
+	vector<int> data(10000);
+	for (int i = 0; i < 10000; i++) data[i] = i + 1;
+	
+	vector<int> partial_sums(4);
+
+	vector<thread> workers;
+	for (int i = 0; i < 4; i++) {
+		workers.push_back(
+			thread(worker_0701_2, data.begin() + i * 2500, data.begin() + (i + 1) * 2500, &partial_sums[i])
+		);
+	}
+	for (int i = 0; i < 4; i++) workers[i].join();
+
+	int total = 0;
+	for (int i = 0; i < 4; i++) total += partial_sums[i];
+	cout << "전체 합: " << total << endl;
+
+	return 0;
+}
