dfd5aecf723997cfd033e9d90bdb5e540d5b41a4
HwaHyang <me@hwahyang.space>
2022-07-05 22:57:48 +0900

    20220705a
    수업내용 백업
    (중간에 노트북 크래시나서 파일 없음)

---
+++ 07m01w/07m01w/0705/Regex.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <regex> // Regular Expression
+using namespace std;
+
+int main_0705_1() {
+	string s = "123";
+	regex number("[0-9]+");
+
+	if (regex_match(s, number)) {
+		cout << "number" << endl;
+	} else {
+		cout << "not number" << endl;
+	}
+
+	cout << endl;
+
+	vector<string> v = { "012", "345", "abc" };
+
+	regex re("[0-9]{3}");
+
+	for (const auto& n : v) {
+		cout << n << ": " << boolalpha << regex_match(n, re) << endl;
+	}
+
+	cout << endl;
+
+	string str1 = "aaa-bb";
+	cout << str1.replace(0, 2, "zz") << endl;
+	// string.replace(시작위치, 길이, 치환)
+
+	return 0;
+}
