856291cbb7c1d8ee73d237516c1c6c6447ac2448
HwaHyang <me@hwahyang.space>
2022-07-13 22:57:11 +0900

    20220713a
    수업내용 백업

---
+++ 07m02w/07m02w/0713/TypeId.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main_0713_1() {
+	// *런타임*에서 개체의 형식을 확인 가능
+	cout << typeid(int).name() << endl;
+	cout << typeid(type_info).name() << endl;
+	//cout << typeid(main).name() << endl;
+	cout << typeid(cout).name() << endl;
+
+	return 0;
+}
---
+++ 07m02w/07m02w/0713/Volatile.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main_0713_2() {
+	volatile int i = 0; // 휘발성, 값을 항상 메모리에 보관 (컴파일러 최적화 X)
+
+	while (i < 10) i++; // [컴파일러 최적화] volatile이 안걸려있으면 컴파일러가 i=10으로 바꿔버림
+
+	printf("%d\n", i);
+
+	return 0;
+}
